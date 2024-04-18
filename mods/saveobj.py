09ed28f7a388c947c31e2b795a0e970e3420bf94
forxxin <165651451+forxxin@users.noreply.github.com>
2024-04-19 00:37:52 +0800

    Update saveobj.py

--- mods/saveobj.py
+++ mods/saveobj.py
@@ -25,13 +25,18 @@ class CacheStr:
 
 import json
 def save_json(file,data):
-    with open(file, 'w', encoding='utf-8') as f:
-        json.dump(data, f, ensure_ascii=False, indent=4)
+    try:
+        with open(file, 'w', encoding='utf-8') as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
+    except:
+        pass
 
 def load_json(file):
-    with open(file, 'r', encoding='utf-8') as f:
-        return json.load(f)
-    
+    try:
+        with open(file, 'r', encoding='utf-8') as f:
+            return json.load(f)
+    except:
+        pass
 # if __name__ == "__main__":
     # s=''
     # saveobj.savestr(cachefile,s)
