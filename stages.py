8cd83d4eb426e3ce938fccaa99182bf0a10a571c
forxxin <165651451+forxxin@users.noreply.github.com>
2024-04-22 01:34:47 +0800

    Update stages.py

--- stages.py
+++ stages.py
@@ -47,14 +47,18 @@ def stages():
             danger_level=danger_level(d.get('dangerLevel')),
             normaldrops=[],
         ) for stageId,d in anhrtags.GData.json_table('stage').get('stages',{}).items()}
+    stageid_tran={}
+    for itemid,stages_all in farmcalc.best_stages().items():
+        for beststages,san in stages_all:
+            for stage in beststages:
+                stageid_tran[stage.id.replace('_perm','')] = stage.id
+            break
     for stageId,stage in res.items():
         activityId = zoneId2activityId().get(stage.zoneId)
         if activityId:
             activity = activitys().get(activityId)
             stage.activity_name=activity.name.replace(' - Rerun','')
-        liststageIds_,liststageIds = farmcalc.best_stages()
-        stageid_tran={b:a for a,b in zip(liststageIds_,liststageIds)}
-        best_stages = [d for s,d in res.items() if d.id in liststageIds]
+        best_stages = [d for s,d in res.items() if d.id in stageid_tran]
         for stage in best_stages:
             stage_id = stageid_tran.get(stage.id)
             stage.normaldrops = [item.name for item in farmcalc.Data.stages[stage_id].normaldrops()]
@@ -80,45 +84,54 @@ def activitys():
         displayType = d.get('displayType'),
     ) for activityId,d in anhrtags.GData.json_table('activity').get('basicInfo',{}).items()}
 
-def print_best():
-    liststageIds_,liststageIds,minimize_stage_key = farmcalc.best_stages()
+def print_best(minimize_stage_key='san'):
+    stageid_tran={}
+    for itemid,stages_all in farmcalc.best_stages(minimize_stage_key=minimize_stage_key).items():
+        for beststages,san in stages_all:
+            for stage in beststages:
+                stageid_tran[stage.id.replace('_perm','')] = stage.id
+            break
     print(f'minimize_stage_key: {minimize_stage_key}')
-    stageid_tran={b:a for a,b in zip(liststageIds_,liststageIds)}
     def stage_str(stage):
         stage_id = stageid_tran.get(stage.id)
         return stage.code+str([item.name for item in farmcalc.Data.stages[stage_id].normaldrops()])
-    best_stages = [d for s,d in stages().items() if d.id in liststageIds]
-    activity_stages={}
+    best_stages = [d for s,d in stages().items() if d.id in stageid_tran]
+    activity_stages={activity.get('name'):[] for activityId, activity in anhrtags.GData.json_table('story_review').items()}
     stageType_stages={}
     for stage in best_stages:
         activityId = zoneId2activityId().get(stage.zoneId)
         if activityId:
             activity = activitys().get(activityId)
             activity_name = activity.name.replace(' - Rerun','')
-            activity_stages.setdefault(activity_name,[]).append(stage)
+            activity_stages.get(activity_name).append(stage)
         else:
             stageType_stages.setdefault(stage.stageType,[]).append(stage)
     for activity_name,stages_ in activity_stages.items():
-        print(f'"{activity_name}"{' '*(36-len(activity_name))}# {' '.join([stage_str(stage) for stage in stages_])}')
+        if stages_:
+            print(f'"{activity_name}"{' '*(36-len(activity_name))}# {' '.join([stage_str(stage) for stage in stages_])}')
     for stageType,stages_ in stageType_stages.items():
         print(f'"{stageType}"{' '*(36-len(stageType))}# {' '.join([stage_str(stage) for stage in stages_])}')
+    return {activity_name:stages_ for activity_name,stages_ in activity_stages.items() if stages_}, stageType_stages
 
 if __name__ == '__main__':
+    # farmcalc.init(server='US',minimize_stage_key='',lang='zh',update=False)
+    farmcalc.init(server='US',minimize_stage_key='',lang='en',update=False)
     print_best()
 
     df = pd.DataFrame(stages().values())
     df.to_excel('stages.xlsx')
     # print(df)
 
-"Stultifera Navis"                    # SN-5['Oriron Shard']
-"Break the Ice"                       # BI-5['Diketon']
-"Dossoles Holiday"                    # DH-4['Ester']
-"A Walk in the Dust"                  # WD-1['Oriron Shard'] WD-5[] WD-6['Polyester Pack'] WD-8['RMA70-12']
-"Who Is Real"                         # WR-3['Frontline Battle Record']
-"Mansfield Break"                     # MB-6['Sugar Pack']
-"Twilight of Wolumonde"               # TW-5['Device'] TW-6['Incandescent Alloy']
-"Heart of Surging Flame"              # OF-F3['Sugar Pack']
+# minimize_stage_key: san
 "Grani and the Knights' Treasure"     # GT-4['Sugar Substitute'] GT-5['Loxic Kohl']
-"MAIN"                                # 0-2['Drill Battle Record'] 1-7['Orirock Cube'] 2-3['Damaged Device'] S2-5['Orirock'] 2-10['RMA70-12'] 3-1['Aketon'] 3-3['Grindstone'] 3-4['Integrated Device'] 3-7['Polyketon'] 3-8['Polyester Pack'] 4-2['Sugar Pack'] 4-8['Grindstone'] 4-9['RMA70-12'] 5-7['Grindstone', 'Oriron'] 5-10['Integrated Device', 'Orirock Cube'] 7-12['Sugar Pack', 'Polyketon'] 7-15['Integrated Device'] 7-18['Oriron Cluster', 'Polyketon'] R8-2['Loxic Kohl'] R8-11['Crystalline Component'] 9-3['Device'] 9-14['Crystalline Component'] 9-16['Grindstone', 'Oriron'] 9-18['Semi-Synthetic Solvent'] 9-19['RMA70-12'] 10-3['Coagulating Gel'] 10-3['Coagulating Gel'] 10-4['Device', 'Aketon'] 10-6['Orirock Cluster'] 10-7['Manganese Ore'] 10-11['Oriron Cluster'] 10-16['Polyester', 'Manganese Ore'] 10-17['Compound Cutting Fluid'] 11-3['Transmuted Salt'] 11-3['Transmuted Salt'] 11-6['Sugar Pack', 'Oriron'] 11-7['Integrated Device'] 11-8['Semi-Synthetic Solvent'] 11-13['Loxic Kohl'] 11-14['Incandescent Alloy'] 11-18['Orirock Cluster'] 12-5['Coagulating Gel'] 12-7['RMA70-12'] 12-9['Tactical Battle Record'] 12-10['Semi-Synthetic Solvent'] 12-12['Incandescent Alloy'] 12-17['Compound Cutting Fluid']
+"Heart of Surging Flame"              # OF-F3['Sugar Pack']
+"Twilight of Wolumonde"               # TW-5['Device'] TW-6['Incandescent Alloy']
+"Mansfield Break"                     # MB-6['Sugar Pack']
+"Who Is Real"                         # WR-3['Frontline Battle Record']
+"A Walk in the Dust"                  # WD-1['Oriron Shard'] WD-5[] WD-6['Polyester Pack'] WD-8['RMA70-12']
+"Dossoles Holiday"                    # DH-4['Ester']
+"Break the Ice"                       # BI-5['Diketon']
+"Stultifera Navis"                    # SN-5['Oriron Shard']
+"MAIN"                                # 0-2['Drill Battle Record'] 1-7['Orirock Cube'] 2-3['Damaged Device'] S2-5['Orirock'] 2-10['RMA70-12'] 3-1['Aketon'] 3-3['Grindstone'] 3-4['Integrated Device'] 3-7['Polyketon'] 3-8['Polyester Pack'] 4-2['Sugar Pack'] 4-8['Grindstone'] 4-9['RMA70-12'] 5-7['Grindstone', 'Oriron'] 5-10['Integrated Device', 'Orirock Cube'] 7-12['Sugar Pack', 'Polyketon'] 7-15['Integrated Device'] 7-18['Oriron Cluster', 'Polyketon'] R8-2['Loxic Kohl'] R8-11['Crystalline Component'] 9-3['Device'] 9-16['Grindstone', 'Oriron'] 9-18['Semi-Synthetic Solvent'] 9-19['RMA70-12'] 10-3['Coagulating Gel'] 10-3['Coagulating Gel'] 10-4['Device', 'Aketon'] 10-6['Orirock Cluster'] 10-7['Manganese Ore'] 10-11['Oriron Cluster'] 10-16['Polyester', 'Manganese Ore'] 10-17['Compound Cutting Fluid'] 11-3['Transmuted Salt'] 11-3['Transmuted Salt'] 11-6['Sugar Pack', 'Oriron'] 11-7['Integrated Device'] 11-7['Integrated Device'] 11-8['Semi-Synthetic Solvent'] 11-13['Loxic Kohl'] 11-14['Incandescent Alloy'] 12-5['Coagulating Gel'] 12-9['Tactical Battle Record'] 12-10['Semi-Synthetic Solvent'] 12-12['Incandescent Alloy'] 12-17['Compound Cutting Fluid']
 "SUB"                                 # S3-1['Sugar'] S3-2['Polyester'] S3-3['Oriron']
 "DAILY"                               # CE-6[] SK-2['Carbon Stick'] SK-3['Carbon Stick', 'Carbon Brick'] SK-5['Carbon Brick', 'Carbon Brick'] LS-6['Tactical Battle Record', 'Strategic Battle Record'] CA-2['Skill Summary - 1'] CA-3['Skill Summary - 2', 'Skill Summary - 1'] CA-5['Skill Summary - 3', 'Skill Summary - 1', 'Skill Summary - 2'] PR-A-1['Defender Chip', 'Medic Chip'] PR-A-2['Defender Chip Pack', 'Medic Chip Pack'] PR-B-1['Sniper Chip', 'Caster Chip'] PR-B-2['Sniper Chip Pack', 'Caster Chip Pack'] PR-C-1['Vanguard Chip', 'Supporter Chip'] PR-C-2['Vanguard Chip Pack', 'Supporter Chip Pack'] PR-D-1['Guard Chip', 'Specialist Chip'] PR-D-2['Guard Chip Pack', 'Specialist Chip Pack']
