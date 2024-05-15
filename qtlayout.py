42fcda7e2269d03eced6e83318f003018e7c4baa
forxxin <165651451+forxxin@users.noreply.github.com>
2024-05-15 17:49:40 +0800

    Add files via upload

--- qtlayout.py
+++ qtlayout.py
@@ -7,14 +7,14 @@ def set_space(layout):
     if hasattr(layout,'setVerticalSpacing'):
         layout.setVerticalSpacing(0)
 class MyGridLayout(QtWidgets.QGridLayout):
-    def __init__(self):
-        super(MyGridLayout, self).__init__()
+    def __init__(self,parent=None):
+        super().__init__(parent)
         set_space(self)
 class MyVBoxLayout(QtWidgets.QVBoxLayout):
-    def __init__(self):
-        super(MyVBoxLayout, self).__init__()
+    def __init__(self,parent=None):
+        super().__init__(parent)
         set_space(self)
 class MyHBoxLayout(QtWidgets.QHBoxLayout):
-    def __init__(self):
-        super(MyHBoxLayout, self).__init__()
+    def __init__(self,parent=None):
+        super().__init__(parent)
         set_space(self)
